79f41f57657b0595cfdd02e9b86067f1868dd1a1
williamorim <thewilliam89@gmail.com>
2024-10-28 19:48:05 -0300

    feito na live

--- shiny-python/partidas/app.py
+++ shiny-python/partidas/app.py
@@ -4,15 +4,27 @@ import pandas as pd
 matches = pd.read_csv("shiny-python/partidas/matches.csv")
 
 times = matches.home.unique().tolist()
+times.sort()
 
-app_ui = ui.page_fluid(
-    ui.panel_title("Últimas 10 partidas de um time"),
-    ui.input_select(
-        id = "time", 
-        label = "Selecione um time",
-        choices = times
+app_ui = ui.page_navbar(
+    ui.nav_panel(
+        "Últimas 10 partidas",
+        ui.layout_sidebar(
+            ui.sidebar(
+                ui.input_select(
+                    id = "time", 
+                    label = "Selecione um time",
+                    choices = times
+                )
+            ),
+            ui.panel_title("Últimas 10 partidas de um time"),
+            ui.output_table("tabela")
+        ),
     ),
-    ui.output_table("tabela")
+    ui.nav_panel(
+        "Número de pontos"
+    ),
+    title="App Partidas"
 )
 
 
